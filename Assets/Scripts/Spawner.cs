b644af56602070611f88a7d6502ac42867547aa4
Fabian <ornelasf1@gmail.com>
2024-06-23 04:07:13 -0500

    Dont overlap foes

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -27,12 +27,19 @@ public class Spawner : MonoBehaviour
         elapsedTime += Time.deltaTime;
         if (elapsedTime > spawnPerSecond) {
             elapsedTime = 0f;
-            
-            float yPos = Random.Range(spawnZone.rect.yMin, spawnZone.rect.yMax);
-            float xPos = Random.Range(spawnZone.rect.xMin, spawnZone.rect.xMax);
-            Vector3 newWorldPoint = spawnZone.TransformPoint(new Vector3(xPos, yPos, -1f));
-            newWorldPoint.z = -1f;
-            Instantiate(enemy, newWorldPoint, Quaternion.identity);
+
+            for (int retries = 0; retries < 3; retries++)
+            {
+                float yPos = Random.Range(spawnZone.rect.yMin, spawnZone.rect.yMax);
+                float xPos = Random.Range(spawnZone.rect.xMin, spawnZone.rect.xMax);
+                Vector3 newWorldPoint = spawnZone.TransformPoint(new Vector3(xPos, yPos, -1f));
+                newWorldPoint.z = -1f;
+                if (!Physics2D.OverlapCircle(newWorldPoint, 2.5f, 1 << LayerMask.NameToLayer("Foe"))) {
+                    Instantiate(enemy, newWorldPoint, Quaternion.identity);
+                    break;
+                }
+            }
+
         }
     }
 }
